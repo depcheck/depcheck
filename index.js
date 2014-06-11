36b717e05511ec9d668a416775560bb45bcc8a1e
Dylan Greene <dylang@gmail.com>
2014-06-10 19:00:22 -0700

    seems to be working with ignorePaths

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var fs = require("fs");
 var path = require("path");
 var esprima = require("esprima");
 var sets = require("simplesets");
+var q = require('q');
+
 var walkdir = require("walkdir");
+var _ = require('lodash');
+var traverse = require('traverse');
+
 
 if (typeof String.prototype.startsWith !== "function") {
   String.prototype.startsWith = function (str) {
@@ -10,6 +15,7 @@ if (typeof String.prototype.startsWith !== "function") {
   };
 }
 
+/*
 function traverse(object, visitor) {
   var key, child;
 
@@ -26,6 +32,7 @@ function traverse(object, visitor) {
     }
   }
 }
+*/
 
 function isRequire(node) {
   return node.callee.name === "require" || (node.callee.property && node.callee.property.name === "loadNpmTasks");
@@ -48,15 +55,17 @@ function parse(filename) {
 }
 
 function checkFile(filename) {
-  var used = new sets.Set();
   var syntax = parse(filename);
 
   if (!syntax) {
-    return new sets.Set();
+    return;
   }
 
-  traverse(syntax, function (node) {
+  var used = [];
+  traverse(syntax).forEach(function (node) {
     var arg;
+
+    if (!node) {return;}
 
     if (node.type !== "CallExpression") {
       return;
@@ -71,21 +80,14 @@ function checkFile(filename) {
     arg = node.arguments[0];
 
     if (arg.type === "Literal" && arg.value[0] !== ".") {
-      used.add(arg.value);
+      used.push(arg.value);
     }
   });
 
   return used;
 }
 
-function hasBin(root, dependency) {
-  try {
-    var depPkg = require(path.join(root, "node_modules", dependency, "package.json"));
-    return depPkg.bin !== undefined;
-  } catch (e) {
-    return false;
-  }
-}
+
 
 function collectUnused(root, usedDependencies, definedDependencies) {
   var found = new sets.Set();
@@ -101,10 +103,9 @@ function collectUnused(root, usedDependencies, definedDependencies) {
   return definedDependencies.difference(found).array();
 }
 
-function check(options, root, files, cb) {
-  var pkg = require(path.join(root, "/package.json"));
-  var deps = new sets.Set(Object.keys(pkg.dependencies));
+function check(files, options) {
   var usedDependencies = new sets.Set();
+
   var ret = {};
 
   files.forEach(function (file) {
@@ -114,62 +115,87 @@ function check(options, root, files, cb) {
   ret.dependencies = collectUnused(root, usedDependencies, deps);
 
   if (!options.withoutDev) {
-    ret.devDependencies = collectUnused(root, usedDependencies, new sets.Set(Object.keys(pkg.devDependencies || {})));
+    ret.devDependencies = collectUnused(root, usedDependencies, devDeps);
   } else {
     ret.devDependencies = [];
   }
 
-  cb(ret);
+  return ret;
 }
 
-function collectSubdirectories(directories, cb) {
-  var n = 0;
-  var files = [];
+function checkDirectory(dir, ignorePaths, deps, devDeps) {
 
-  directories.forEach(function (dir) {
-    var finder = walkdir(dir);
+  var deferred = q.defer();
 
-    n++;
+  var directoryPromises = [];
 
-    finder.on("file", function (file) {
-      files.push(file);
-    });
-
-    finder.on("end", function () {
-      n--;
-      if (n === 0) {
-        cb(files);
-      }
-    });
-  });
-}
-
-module.exports = function checkDirectory(dir, options, cb) {
-  var files = [];
-  var directories = [];
   var finder = walkdir(dir, { "no_recurse": true });
 
-  finder.on("directory", function (dir) {
-    if (path.basename(dir) === "node_modules") {
-      return;
+  finder.on("directory", function (subdir) {
+    if (ignorePaths.contains(path.basename(subdir))
+      || (deps.isEmpty() && devDeps.isEmpty()))  {
+        return;
     }
-    directories.push(dir);
+
+    directoryPromises.push(checkDirectory(subdir, ignorePaths, deps, devDeps));
   });
 
   finder.on("file", function (file) {
-    if (path.extname(file) !== ".js") {
-      return;
+    if (path.extname(file) === ".js") {
+      deps = deps.difference(checkFile(file));
+      devDeps = devDeps.difference(checkFile(file));
     }
-    files.push(file);
   });
 
   finder.on("end", function () {
-    if (directories.length === 0) {
-      check(options, dir, files, cb);
-    } else {
-      collectSubdirectories(directories, function (subFiles) {
-        check(options, dir, files.concat(subFiles), cb);
+    deferred.resolve(q.allSettled(directoryPromises).then(function(directoryResults) {
+
+      _(directoryResults).each(function(result) {
+        if (result.state = 'fulfilled') {
+          deps = deps.intersection(result.value.dependencies);
+          devDeps = devDeps.intersection(result.value.devDependencies);
+        }
       });
-    }
+
+      return {
+        dependencies: deps.valueOf(),
+        devDependencies: devDeps.valueOf()};
+    }));
   });
-};
+
+  return deferred.promise;
+}
+
+function depCheck(rootDir, options, cb) {
+  var pkg = require(path.join(rootDir, 'package.json'));
+  var deps = filterDependencies(pkg.dependencies);
+  var devDeps = filterDependencies(options.withoutDev ? [] : pkg.devDependencies);
+  var ignorePaths = _([
+    '.git',
+    '.svn',
+    '.hg',
+    '.idea',
+    'node_modules'
+  ])
+    .concat(options.ignorePaths)
+    .flatten()
+    .unique();
+
+  function hasBin(dependency) {
+    try {
+      var depPkg = require(path.join(rootDir, "node_modules", dependency, "package.json"));
+      return _.has(depPkg, 'bin');
+    } catch (e) {}
+  }
+
+  function filterDependencies(dependencies) {
+    return _(dependencies)
+      .keys()
+      .reject(hasBin);
+  }
+
+  return checkDirectory(rootDir, ignorePaths, deps, devDeps)
+    .then(cb).done();
+}
+
+module.exports = depCheck;
