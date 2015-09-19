woodb/depcheck-es6

Add a test case to assert that custom extensions is not supported.

test/index.js
@@ -94,6 +94,15 @@ describe("depcheck", function () {
     });
   });
 
+  it("should accept extensions for options", function testNested(done) {
+    var absolutePath = path.resolve("test/fake_modules/extensions");
+
+    depcheck(absolutePath, { extensions: ['.es'] }, function checked(unused) {
+      assert.equal(unused.dependencies.length, 0);
+      done();
+    });
+  });
+
   it("should allow dynamic package metadata", function testDynamic(done) {
     var absolutePath = path.resolve("test/fake_modules/bad");
 

depcheck/depcheck

test/index.js
@@ -137,6 +137,15 @@ describe("depcheck", function () {
     });
   });
 
+  it("should accept extensions for options", function testExtensions(done) {
+    var absolutePath = path.resolve("test/fake_modules/extensions");
+
+    depcheck(absolutePath, { extensions: ['.es'] }, function checked(unused) {
+      assert.equal(unused.dependencies.length, 0);
+      done();
+    });
+  });
+
   it("should allow dynamic package metadata", function testDynamic(done) {
     var absolutePath = path.resolve("test/fake_modules/bad");
 