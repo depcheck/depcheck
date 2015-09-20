875768bcbfebea5b7683549fbc433a6bdd5a3aed
Junle <lijunle@gmail.com>
2015-09-21 00:52:13 +0800

    Store syntax-error files to invalidFiles property.
    - This is the rumpl's depcheck behavior. Be compatible with it.
    - Add test case to cover scenario.

--- test/fake_modules/bad_js/deeper/also_bad_js.js
+++
@@ -1,7 +0,0 @@
-
-// there's a ) missing which will make it impossible parse
-;["util","assert"].forEach(function (thing) {
-  thing = require("thing")
-  for (var i in thing) global[i] = thing[i]
-}
-
