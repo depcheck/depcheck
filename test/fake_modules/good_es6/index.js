fc1a034c8d6a3fe0db68cb157e4796c8365265fa
Junle <lijunle@gmail.com>
2015-09-23 00:22:30 +0800

    Resolve comma-spacing.

--- test/fake_modules/good_es6/index.js
+++ test/fake_modules/good_es6/index.js
@@ -7,7 +7,7 @@ import name from "name-import";
 import * as star from "star-import";
 import { member } from "member-import";
 import { foobar as barfoo } from "member-alias-import";
-import { foo , bar } from "multiple-member-import";
+import { foo, bar } from "multiple-member-import";
 import { a, b as c } from "mixed-member-alias-import";
 import d, { e } from "mixed-name-memeber-import";
 import h, * as i from "mixed-default-star-import";
