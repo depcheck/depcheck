dc74d8fcaf4b44f1f2e7e86e813ddc21ca0cb313
Junle <lijunle@gmail.com>
2015-09-30 00:12:20 +0800

    Add ESlint special parser.
    - Fix a comma in `.eslintrc` file.
    - Add eslint special parser to self check.
    - Now, there is only one explicit ignore package - mocha-lcov-reporter

---
+++ src/special/eslint.js
@@ -0,0 +1,39 @@
+import path from 'path';
+
+function wrapToArray(obj) {
+  if (!obj) {
+    return [];
+  } else if (obj instanceof Array) {
+    return obj;
+  }
+
+  return [obj];
+}
+
+function checkAirbnb(configs) {
+  if (configs.indexOf('airbnb') !== -1) {
+    return ['eslint-config-airbnb', 'babel-eslint', 'eslint-plugin-react', 'eslint'];
+  } else if (configs.indexOf('airbnb/base') !== -1) {
+    return ['eslint-config-airbnb', 'babel-eslint', 'eslint'];
+  } else if (configs.indexOf('airbnb/legacy') !== -1) {
+    return ['eslint-config-airbnb', 'eslint'];
+  }
+
+  return false;
+}
+
+export default (content, filename, deps, dir) => {
+  const basename = path.basename(filename);
+  if (basename === '.eslintrc') {
+    const configs = wrapToArray(JSON.parse(content).extends);
+
+    const airbnbResult = checkAirbnb(configs);
+    if (airbnbResult) {
+      return airbnbResult;
+    }
+
+    return [];
+  }
+
+  return [];
+};
