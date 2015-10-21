27ca41e45b2a14587c60e3a9f218b488125e16a5
JunLe Li <lijunle@gmail.com>
2015-10-21 07:19:16 +0000

    Extract Travis related variables to a function.
    - Make unittest and future maintain easier.

--- src/utils/web-report.js
+++ src/utils/web-report.js
@@ -1,18 +1,38 @@
 import request from 'request';
 
-export default function postWebReport(result, log, error, env, { webReport, webService }) {
-  // TODO this web report feature only support Travis CI and GitHub now
-  return webReport === undefined || env.TRAVIS !== 'true'
-  ? Promise.resolve(result)
-  : new Promise(resolve =>
+function getSettings(env) {
+  // TODO web report only supports Travis CI and GitHub now
+  if (env.TRAVIS === 'true') {
+    return {
+      url: `/github/${env.TRAVIS_REPO_SLUG}`,
+      branch: env.TRAVIS_BRANCH,
+    };
+  }
+
+  return {};
+}
+
+export default function postWebReport(
+  result, log, error, env, { webReport, webService }) {
+  if (webReport === undefined) {
+    return Promise.resolve(result);
+  }
+
+  const { url, branch } = getSettings(env);
+  if (!url) {
+    error('Build environment is not supported yet, please report issue to https://github.com/lijunle/depcheck-es6');
+    return Promise.resolve(result);
+  }
+
+  return new Promise(resolve =>
     request({
       baseUrl: webService,
-      url: `/github/${env.TRAVIS_REPO_SLUG}`,
+      url,
       method: 'POST',
       json: true,
       body: {
         token: env.DEPCHECK_TOKEN,
-        branch: env.TRAVIS_BRANCH,
+        branch,
         report: webReport,
         result,
       },
