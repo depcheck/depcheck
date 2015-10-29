9b26e19d8f5203ae55bdb1fc8770cf9ea42f00ac
JunLe Li <lijunle@gmail.com>
2015-10-29 11:19:16 +0000

    Leverage babylon to support ES7 syntax.
    - Babylon is the parser for Babel. All Babel syntax is supported.
    - Babylon is inspired from acorn and acorn-jsx, and cover their
      functionalities. So, remove acorn and acorn-jsx dependencies now.

--- src/parser/es6.js
+++ src/parser/es6.js
@@ -1,8 +1,23 @@
-import * as acorn from 'acorn';
+import { parse } from 'babylon';
+
+export const config = {
+  sourceType: 'module',
+  features: {
+    // Enable all possible experimental features.
+    // Because we only parse them, not evaluate any code, it is safe to do so.
+    // Reference Babel docs: https://babeljs.io/docs/usage/experimental/
+    'es7.asyncFunctions': true,
+    'es7.classProperties': true,
+    'es7.comprehensions': true,
+    'es7.decorators': true,
+    'es7.doExpressions': true,
+    'es7.exponentiationOperator': true,
+    'es7.exportExtensions': true,
+    'es7.functionBind': true,
+    'es7.objectRestSpread': true,
+    'es7.trailingFunctionCommas': true,
+  },
+};
 
 export default content =>
-  acorn.parse(content, {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    allowHashBang: true,
-  });
+  parse(content, config);
