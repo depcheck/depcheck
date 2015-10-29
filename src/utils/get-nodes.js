b8f09d2e01cbedb8eea2fe0d4d68c9e68a6737e5
Junle <lijunle@gmail.com>
2015-10-30 02:06:43 +0800

    Handle AST walker by hand.
    - Remove node-source-walk dependency.
    - Implement get all nodes from an AST in `utils/get-nodes`.

---
+++ src/utils/get-nodes.js
@@ -0,0 +1,24 @@
+function recursive(ast, visited) {
+  const nodes = [];
+
+  if (ast && ast.type && !visited.has(ast)) {
+    visited.add(ast);
+    nodes.push(ast);
+  }
+
+  if (Array.isArray(ast)) {
+    return nodes.concat(...ast.map(node => recursive(node, visited)));
+  } else if (ast && typeof ast === 'object') {
+    return nodes.concat(...Object.keys(ast)
+      .filter(key => key !== 'tokens' && key !== 'comments')
+      .map(key => recursive(ast[key], visited)));
+  }
+
+  return nodes;
+}
+
+export default function getNodes(ast) {
+  const visited = new WeakSet();
+  const nodes = recursive(ast, visited);
+  return nodes;
+}
