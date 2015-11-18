8e8d18cc4eb15555a081d28a6fdcde7c1c58eb9e
Junle Li <lijunle@gmail.com>
2015-11-19 00:53:46 +0800

    Convert the ESLint special parser test to data-driven.

--- test/special/eslint.js
+++ test/special/eslint.js
@@ -3,55 +3,70 @@
 import 'should';
 import eslintSpecialParser from '../../src/special/eslint';
 
+const testCases = [
+  {
+    name: 'ignore when user not extends any config in `.eslintrc`',
+    content: {},
+    expected: [],
+  },
+  {
+    name: 'ignore when `airbnb` is not used `.eslintrc`',
+    content: {
+      extends: 'others',
+    },
+    expected: [],
+  },
+  {
+    name: 'handle the `airbnb` config',
+    content: {
+      extends: 'airbnb',
+    },
+    expected: [
+      'eslint-config-airbnb',
+      'eslint-plugin-react',
+    ],
+  },
+  {
+    name: 'handle the `airbnb/base` config',
+    content: {
+      extends: 'airbnb/base',
+    },
+    expected: [
+      'eslint-config-airbnb',
+    ],
+  },
+  {
+    name: 'handle the `airbnb/legacy` config',
+    content: {
+      extends: 'airbnb/legacy',
+    },
+    expected: [
+      'eslint-config-airbnb',
+    ],
+  },
+  {
+    name: 'detect `airbnb` even multiple configs are used',
+    content: {
+      extends: ['airbnb', 'others'],
+    },
+    expected: [
+      'eslint-config-airbnb',
+      'eslint-plugin-react',
+    ],
+  },
+];
+
 describe('eslint special parser', () => {
   it('should ignore when filename is not `.eslintrc`', () => {
     const result = eslintSpecialParser('content', '/a/file');
     result.should.deepEqual([]);
   });
 
-  it('should ignore when user not extends any config in `.eslintrc`', () => {
-    const content = JSON.stringify({});
-    const result = eslintSpecialParser(content, '/path/to/.eslintrc');
-
-    result.should.have.length(0);
-  });
-
-  it('should ignore when `airbnb` is not used `.eslintrc`', () => {
-    const content = JSON.stringify({ extends: 'others' });
-    const result = eslintSpecialParser(content, '/path/to/.eslintrc');
-
-    result.should.have.length(0);
-  });
-
-  it('should handle the `airbnb` config', () => {
-    const content = JSON.stringify({ extends: 'airbnb' });
-    const result = eslintSpecialParser(content, '/path/to/.eslintrc');
-
-    result.should.have.length(2);
-    result.should.containEql('eslint-config-airbnb');
-    result.should.containEql('eslint-plugin-react');
-  });
-
-  it('should handle the `airbnb/base` config', () => {
-    const content = JSON.stringify({ extends: 'airbnb/base' });
-    const result = eslintSpecialParser(content, '/path/to/.eslintrc');
-
-    result.should.have.length(1);
-    result.should.containEql('eslint-config-airbnb');
-  });
-
-  it('should handle the `airbnb/legacy` config', () => {
-    const content = JSON.stringify({ extends: 'airbnb/legacy' });
-    const result = eslintSpecialParser(content, '/path/to/.eslintrc');
-
-    result.should.have.length(1);
-    result.should.containEql('eslint-config-airbnb');
-  });
-
-  it('should detect `airbnb` even multiple configs are used', () => {
-    const content = JSON.stringify({ extends: ['airbnb', 'others'] });
-    const result = eslintSpecialParser(content, '/path/to/.eslintrc');
-
-    result.should.have.length(2);
-  });
+  describe('with JSON format', () =>
+    testCases.forEach(testCase =>
+      it(`should ${testCase.name}`, () => {
+        const content = JSON.stringify(testCase.content);
+        const result = eslintSpecialParser(content, '/path/to/.eslintrc');
+        result.should.deepEqual(testCase.expected);
+      })));
 });
