5ed68086a4fbf4c222fa7d55a0b018104df3bb0d
Junle Li <lijunle@gmail.com>
2015-11-21 19:19:25 +0800

    Change setup/cleanup logic to async.

--- test/special/webpack.js
+++ test/special/webpack.js
@@ -1,8 +1,8 @@
 /* global describe, it */
 
 import 'should';
-import fs from 'fs';
 import path from 'path';
+import fsp from 'fs-promise';
 import parse from '../../src/special/webpack';
 
 const testCases = [
@@ -75,24 +75,24 @@ const testCases = [
   },
 ];
 
-function getTempPath(filename, content) {
+async function getTempPath(filename, content) {
   const tempFolder = path.resolve(__dirname, `temp-${Date.now()}`);
   const tempPath = path.resolve(tempFolder, filename);
-  fs.mkdirSync(tempFolder);
-  fs.writeFileSync(tempPath, content);
+  await fsp.mkdir(tempFolder);
+  await fsp.writeFile(tempPath, content);
   return tempPath;
 }
 
-function removeTempFile(filepath) {
+async function removeTempFile(filepath) {
   const fileFolder = path.dirname(filepath);
-  fs.unlinkSync(filepath);
-  fs.rmdirSync(fileFolder);
+  await fsp.unlink(filepath);
+  await fsp.rmdir(fileFolder);
 }
 
-function testWebpack(filename, content, deps, expectedDeps) {
-  const tempPath = getTempPath(filename, content);
+async function testWebpack(filename, content, deps, expectedDeps) {
+  const tempPath = await getTempPath(filename, content);
   const result = parse(content, tempPath, deps, __dirname);
-  removeTempFile(tempPath);
+  await removeTempFile(tempPath);
   Array.from(result).should.deepEqual(expectedDeps);
 }
 
@@ -106,19 +106,19 @@ describe('webpack special parser', () => {
     const config = JSON.stringify({ module: testCases[0].module });
     const content = `module.exports = ${config}`;
     const deps = testCases[0].deps.concat(['unused-loader']);
-    testWebpack('webpack.config.js', content, deps, testCases[0].deps);
+    return testWebpack('webpack.config.js', content, deps, testCases[0].deps);
   });
 
   it('should handle require call to other modules', () => {
     const config = JSON.stringify({ module: testCases[0].module });
     const content = `module.exports = ${config}\nrequire('webpack')`;
-    testWebpack('webpack.config.js', content, testCases[0].deps, testCases[0].deps);
+    return testWebpack('webpack.config.js', content, testCases[0].deps, testCases[0].deps);
   });
 
   testCases.forEach(testCase =>
     it(`should ${testCase.name} in configuration file`, () => {
       const config = JSON.stringify({ module: testCase.module });
       const content = `module.exports = ${config}`;
-      testWebpack('webpack.config.js', content, testCase.deps, testCase.deps);
+      return testWebpack('webpack.config.js', content, testCase.deps, testCase.deps);
     }));
 });
