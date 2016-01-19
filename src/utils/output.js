0f7157fa61f06d3665e9450640146fe317142ac4
Gord Tanner <gord@bithound.io>
2016-01-19 13:29:37 -0500

    added an option to report on used dependencies
    - used param will now output the used deps
    - regular output will show a list of the used deps
    - json output will show the files that the deps are used

--- src/utils/output.js
+++ src/utils/output.js
@@ -17,17 +17,23 @@ function prettify(caption, deps) {
   return list.length ? [caption].concat(list) : [];
 }
 
-export default function output(result, log, json) {
+export default function output(result, log, argv) {
   return new Promise(resolve => {
-    if (json) {
-      log(JSON.stringify(result, replacer));
+    if (argv.json) {
+      var clone = Object.assign({}, result);
+      if (!argv.used) delete clone.used;
+      log(JSON.stringify(clone, replacer));
     } else if (noIssue(result)) {
-      log('No depcheck issue');
+      const unused = ['No depcheck issue'];
+      const used = prettify('Used Dependencies', Object.keys(result.used));
+      const content = unused.concat(argv.used ? used : []).join('\n');
+      log(content);
     } else {
       const deps = prettify('Unused dependencies', result.dependencies);
       const devDeps = prettify('Unused devDependencies', result.devDependencies);
       const missing = prettify('Missing dependencies', result.missing);
-      const content = deps.concat(devDeps, missing).join('\n');
+      const used = prettify('Used Dependencies', Object.keys(result.used));
+      const content = deps.concat(devDeps, missing, argv.used ? used : []).join('\n');
 
       log(content);
     }
