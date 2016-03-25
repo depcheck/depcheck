85eee0a97e8fd3b14b4d332470b07475c28fa5e1
Junle Li <lijunle@gmail.com>
2016-03-26 03:47:52 +0800

    Implement the logic for require calls.
    - There are three require call pattern. All are supported.
    - Do crazy copy-paste on the code to make it work.
    - It is a MUST to do refactor after get all test cases passed.

--- src/special/gulp-load-plugins.js
+++ src/special/gulp-load-plugins.js
@@ -7,10 +7,10 @@ import importDetector from '../detector/importDeclaration';
 import requireDetector from '../detector/requireCallExpression';
 
 /**
- * Get the reference paths to the imported variable.
- * @example With code `import loadPlugins from 'x'`, returns references to `loadPlugins` variable.
+ * Get the variable name for the imported package.
+ * @example With code `import loadPlugins from 'x'`, returns the 'loadPlugins' string.
  */
-function getImportReferences(path) {
+function getImportVariableName(path) {
   // check if import the default exported value
   const [importDefaultSpecifier] = path.get('specifiers');
   if (!importDefaultSpecifier.isImportDefaultSpecifier()) {
@@ -24,6 +24,14 @@ function getImportReferences(path) {
   }
 
   const variableName = identifier.node.name;
+  return variableName;
+}
+
+/**
+ * Get the references to the variable in the path scope.
+ * @example Within the path scope, returns references to `loadPlugins` variable.
+ */
+function getReferences(path, variableName) {
   const bindings = path.scope.getBinding(variableName);
   const references = bindings.referencePaths;
 
@@ -72,10 +80,13 @@ findMemberExpression.recurse = (path) =>
  * Convert the gulp plugin call to corresponding npm package names
  * @example With string `$.that.plugin`, returns `@that/gulp-plugin` as package name.
  */
-function convertToPackageName(code) {
+function convertToPackageName(code, deps) {
   const parts = code.split('.');
   if (parts.length === 2) { // $.jshint
-    // TODO do it later
+    const name = lodash.kebabCase(parts[1]);
+    const candidates = [`gulp-${name}`, `gulp.${name}`];
+    const dep = lodash.intersection(candidates, deps)[0];
+    return dep;
   } else if (parts.length === 3) { // $.scope.plugin
     const scope = parts[1];
     const name = lodash.kebabCase(parts[2]);
@@ -102,7 +113,8 @@ export default function parseGulpPlugins(content, filePath, deps, rootDir) {
 
       // check if import from 'gulp-load-plugins'
       if (importPackage === 'gulp-load-plugins') {
-        const importReferences = getImportReferences(path);
+        const importVariableName = getImportVariableName(path);
+        const importReferences = getReferences(path, importVariableName);
         const identifierReferences = lodash(importReferences)
           .map(getIdentifierReferences)
           .flatten()
@@ -111,14 +123,65 @@ export default function parseGulpPlugins(content, filePath, deps, rootDir) {
         identifierReferences.forEach(reference => {
           const memberExpression = findMemberExpression(reference);
           const code = content.slice(memberExpression.node.start, memberExpression.node.end);
-          const packageName = convertToPackageName(code);
+          const packageName = convertToPackageName(code, deps);
           results.push(packageName);
         });
       }
 
       // check if require gulp-load-plugins package
       if (requirePackage === 'gulp-load-plugins') {
-        // TODO implement
+        if (
+          path.parentPath.isVariableDeclarator() &&
+          path.parentPath.get('id').isIdentifier()
+        ) {
+          // Pattern: const plugins = require('gulp-load-plugins')
+          const requireVariableName = path.parentPath.get('id').node.name;
+          const requireReferences = getReferences(path, requireVariableName);
+
+          // TODO the following code is copied from import part. Refactor the code.
+          const identifierReferences = lodash(requireReferences)
+            .map(getIdentifierReferences)
+            .flatten()
+            .value();
+
+          identifierReferences.forEach(reference => {
+            const memberExpression = findMemberExpression(reference);
+            const code = content.slice(memberExpression.node.start, memberExpression.node.end);
+            const packageName = convertToPackageName(code, deps);
+            results.push(packageName);
+          });
+        } else if (
+          path.parentPath.isCallExpression() &&
+          path.parentPath.parentPath.isVariableDeclarator() &&
+          path.parentPath.parentPath.get('id').isIdentifier()
+        ) {
+          // Pattern: const $ = require('gulp-load-plugins')()
+          const requireVariableName = path.parentPath.parentPath.get('id').node.name;
+
+          // TODO copy from getIdentifierReferences, may avoid duplicate.
+          const binding = path.scope.getBinding(requireVariableName);
+          const identifierReferences = binding.referencePaths;
+
+          // TODO refactor to avoid duplicate.
+          identifierReferences.forEach(reference => {
+            const memberExpression = findMemberExpression(reference);
+            const code = content.slice(memberExpression.node.start, memberExpression.node.end);
+            const packageName = convertToPackageName(code, deps);
+            results.push(packageName);
+          });
+        } else if (
+          path.parentPath.isCallExpression() &&
+          path.parentPath.parentPath.isMemberExpression()
+        ) {
+          // Pattern: require('gulp-load-plugins')().thisPlugin()
+          const identifierExpression = path.parentPath;
+          const memberExpression = path.parentPath.parentPath;
+
+          // Skip the `require('gulp-load-plugins')()` blob.
+          const code = content.slice(identifierExpression.node.end, memberExpression.node.end);
+          const packageName = convertToPackageName(code, deps);
+          results.push(packageName);
+        }
       }
     },
   });
