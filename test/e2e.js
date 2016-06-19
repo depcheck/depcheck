fc417857fe92d472670ce58a493ed2960975ef70
Junle Li <lijunle@gmail.com>
2016-06-20 02:55:40 +0800

    Refine the E2E test cases.
    - Use spawnSync instead of exec.
    - Show the same local variables.

--- test/e2e.js
+++ test/e2e.js
@@ -2,22 +2,18 @@
 
 import should from 'should';
 import path from 'path';
-import { exec, spawnSync } from 'child_process';
+import { spawnSync } from 'child_process';
+
+const node = process.argv[0];
+const depcheck = path.resolve(__dirname, '../bin/depcheck');
 
 function testE2E(module, output) {
-  return new Promise(resolve => {
-    const binary = path.resolve(__dirname, '../bin/depcheck');
-    const modulePath = path.resolve(__dirname, 'fake_modules', module);
-
-    exec(`node ${binary} ${modulePath}`, (error, stdout, stderr) => {
-      const expectedStdOut = output.map(line => `${line}\n`).join('');
-
-      stdout.should.equal(expectedStdOut);
-      stderr.should.be.empty();
-
-      resolve();
-    });
-  });
+  const expectedStdOut = output.map(line => `${line}\n`).join('');
+  const modulePath = path.resolve(__dirname, 'fake_modules', module);
+  const result = spawnSync(node, [depcheck, modulePath]);
+  should(result.error).be.undefined();
+  result.stdout.toString().should.equal(expectedStdOut);
+  result.stderr.toString().should.be.empty();
 }
 
 describe('depcheck end-to-end', () => {
@@ -31,12 +27,10 @@ describe('depcheck end-to-end', () => {
     testE2E('missing', ['Missing dependencies', '* missing-dep']));
 
   it('should output error exit code when spawned', () => {
-    const node = process.argv[0];
-    const depcheck = path.resolve(__dirname, '../bin/depcheck');
-    const cp = spawnSync(node, [depcheck, './not/exist/folder']);
-
-    should(cp.error).be.undefined();
-    cp.stderr.toString().should.containEql('/not/exist/folder').and.containEql('not exist');
-    cp.status.should.not.equal(0);
+    const result = spawnSync(node, [depcheck, './not/exist/folder']);
+    should(result.error).be.undefined();
+    result.stdout.toString().should.be.empty();
+    result.stderr.toString().should.containEql('/not/exist/folder').and.containEql('not exist');
+    result.status.should.not.equal(0);
   });
 });
