acef2d0e0f27d2f79333a1dd25f92e9c02820509
Jeroen Akkerman <jeroen.akkerman@easyflex.nl>
2017-09-20 17:51:14 +0200

    Fix linting and test errors

--- src/special/package.js
+++ src/special/package.js
@@ -1,17 +1,17 @@
 export default function parsePackageJson(content, filepath, deps) {
-
   // Mark dependencies as "used" if they are used in a script in package.json.
   if (filepath.endsWith('/package.json')) {
     // Grab just the scripts section.
-    const scriptsText = JSON.stringify(JSON.parse(content)['scripts']);
-    const depsInScripts = [];
-    for (const dependency of deps) {
-      if (scriptsText.indexOf(dependency) !== -1) {
-        depsInScripts.push(dependency);
+    const scriptsText = JSON.stringify(JSON.parse(content).scripts);
+    if (scriptsText) {
+      const depsInScripts = [];
+      for (let i = 0, len = deps.length; i < len; i + 1) {
+        if (scriptsText.indexOf(deps[i]) !== -1) {
+          depsInScripts.push(deps[i]);
+        }
       }
+      return depsInScripts;
     }
-    return depsInScripts;
   }
-
   return [];
 }
