b23036a3320b64811d91b657ed9e1c1845d4caf5
Marc Khouri <marc@khouri.ca>
2018-09-02 20:25:20 -0400

    lint

--- src/detector/exportDeclaration.js
+++ src/detector/exportDeclaration.js
@@ -1,7 +1,7 @@
 export default function detectExportDeclaration(node) {
   return (node.type === 'ExportNamedDeclaration'
         || node.type === 'ExportAllDeclaration'
-    )
+  )
     && node.source
     && node.source.value
     ? [node.source.value]
