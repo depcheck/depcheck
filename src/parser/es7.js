9a857df9447e3014dca5ee1efab143dfee9ed54e
Petter Häggholm <petter@petterhaggholm.net>
2018-10-30 16:30:08 -0700

    Update default Babylon plugins

--- src/parser/es7.js
+++ src/parser/es7.js
@@ -1,9 +1,35 @@
 import { parse } from '@babel/parser';
 
+const defaultPlugins = [
+  'estree',
+  'asyncFunctions',
+  'asyncGenerators',
+  'classConstructorCall',
+  'classProperties',
+  'doExpressions',
+  'exponentiationOperator',
+  'exportDefaultFrom',
+  'exportNamespaceFrom',
+  'functionBind',
+  'functionSent',
+  'objectRestSpread',
+  'trailingFunctionCommas',
+  'dynamicImport',
+  'numericSeparator',
+  'optionalChaining',
+  'importMeta',
+  'classPrivateProperties',
+  'bigInt',
+  'optionalCatchBinding',
+  'throwExpressions',
+  'nullishCoalescingOperator',
+  'logicalAssignment',
+];
+
 export default function parseES7(content, filename, deps, dir, parserOpts) {
   // Enable all possible babylon plugins by default.
   // Because we only parse them, not evaluate any code, it is safe to do so.
-  let plugins = ['*'];
+  let plugins = defaultPlugins;
   if (parserOpts && parserOpts.es7 && parserOpts.es7.plugins) {
     plugins = parserOpts.es7.plugins;
   }
--- src/parser/jsx.js
+++ src/parser/jsx.js
@@ -1,9 +1,36 @@
 import { parse } from '@babel/parser';
 
+const defaultPlugins = [
+  'jsx',
+  'estree',
+  'asyncFunctions',
+  'asyncGenerators',
+  'classConstructorCall',
+  'classProperties',
+  'doExpressions',
+  'exponentiationOperator',
+  'exportDefaultFrom',
+  'exportNamespaceFrom',
+  'functionBind',
+  'functionSent',
+  'objectRestSpread',
+  'trailingFunctionCommas',
+  'dynamicImport',
+  'numericSeparator',
+  'optionalChaining',
+  'importMeta',
+  'classPrivateProperties',
+  'bigInt',
+  'optionalCatchBinding',
+  'throwExpressions',
+  'nullishCoalescingOperator',
+  'logicalAssignment',
+];
+
 export default function parseJSX(content, filename, deps, dir, parserOpts) {
   // Enable all possible babylon plugins by default.
   // Because the guys using React always want the newest syntax.
-  let plugins = ['*', 'jsx'];
+  let plugins = defaultPlugins;
   if (parserOpts && parserOpts.jsx && parserOpts.jsx.plugins) {
     plugins = parserOpts.jsx.plugins;
   }
