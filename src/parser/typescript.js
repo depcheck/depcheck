5e9131f42a79c03d6572da29167ea939349d2b25
ilrilan <ilrilan2@gmail.com>
2019-10-21 17:46:03 +0500

    Улучшена обработка typescript

--- src/parser/typescript.js
+++ src/parser/typescript.js
@@ -3,30 +3,21 @@ import { tryRequire } from '../utils';
 
 const typescript = tryRequire('typescript');
 
-export default function parseTypescript(content, filePath) {
+export default function parseTypescript(content) {
   if (!typescript) {
     return [];
   }
-
-  const defaultCompileOptions = {
-    module: typescript.ModuleKind.CommonJS,
-    target: typescript.ScriptTarget.Latest,
-    jsx: typescript.JsxEmit.React,
-  };
-
-  const result = typescript.transpile(
-    content,
-    defaultCompileOptions,
-    filePath,
-  );
 
   // TODO avoid parse source file twice, use Typescript native traverser to find out dependencies.
   // Reference: https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#traversing-the-ast-with-a-little-linter
   // Enable all known compatible @babel/parser plugins at the time of writing.
   // Because we only parse them, not evaluate any code, it is safe to do so.
   // note that babel/parser 7+ does not support *, due to plugin incompatibilities
-  return parse(result, {
+  return parse(content, {
     sourceType: 'module',
+    presets: [
+      '@babel/preset-typescript',
+    ],
     plugins: [
       'asyncGenerators',
       'bigInt',
---
+++ test/fake_modules/typescript_unused/index.ts
@@ -0,0 +1,10 @@
+import('function-import/src/path');
+import a from 'import-with-from/src/path';
+var b = require('require-from/src/path');
+var c = require('@my-namespace/lib')
+var fn2 = require('@my-namespace/lib2/path/in/package')
+
+import { fn3 } from '@my-namespace/lib3'
+import { fn4 } from '@my-namespace/lib4/path/in/package'
+import fn5 from '@my-namespace/lib5'
+import fn6 from '@my-namespace/lib6/path/in/package'
