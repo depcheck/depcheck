296c456c4a219245e5b995d59a1de2b9d9c339c8
Stéphane Veyret <stephane@neptura.org>
2019-10-31 22:01:42 +0100

    TypeScript: detect node built-in type usage
    Issue #444

--- src/utils/typescript.js
+++ src/utils/typescript.js
@@ -1,10 +1,17 @@
+import lodash from 'lodash';
+import builtInModules from 'builtin-modules';
+
 /* eslint-disable import/prefer-default-export */
 const orgDepRegex = /@(.*?)\/(.*)/;
 
 // The name of the DefinitelyTyped package for a given package
 export function getAtTypesName(dep) {
-  const match = orgDepRegex.exec(dep);
-  const pkgName = match ? `${match[1]}__${match[2]}` : dep;
-
+  let pkgName;
+  if (lodash.includes(builtInModules, dep)) {
+    pkgName = 'node';
+  } else {
+    const match = orgDepRegex.exec(dep);
+    pkgName = match ? `${match[1]}__${match[2]}` : dep;
+  }
   return `@types/${pkgName}`;
 }
