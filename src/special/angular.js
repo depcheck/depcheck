58cd6123a4ccc8b46802f933893effcfd0e1f26a
rjlopezdev <rjlopezmad@gmail.com>
2020-02-28 17:04:50 +0100

    feat: add special `angular` detector

---
+++ src/special/angular.js
@@ -0,0 +1,43 @@
+import * as path from 'path';
+import { getContent } from '../utils/file';
+
+const ANGULAR_FILES = ['angular-cli.json', 'angular.json'];
+const ANGULAR_JSON_KEYS = ['scripts', 'styles', 'assets'];
+
+const traverse = (obj, candidates) => {
+  obj.keys((k) => {
+    if (obj[k] && typeof obj[k] === 'object') {
+      if (ANGULAR_JSON_KEYS.includes(k)) {
+        candidates.push(obj[k]);
+      }
+      traverse(obj[k], candidates);
+    }
+  });
+};
+
+export default async function parseAngular(filename) {
+  const basename = path.basename(filename);
+
+  if (ANGULAR_FILES.includes(basename)) {
+    const content = await getContent(filename);
+    const angularJson = JSON.parse(content);
+
+    let candidates = [];
+    traverse(angularJson, candidates);
+    candidates = candidates.flat();
+
+    let dependencies = [];
+    candidates.forEach((candidate) => {
+      const match = candidate.match(
+        /node_modules\/([a-zA-Z-])*|@([a-zA-Z-])*\/([a-zA-Z-]*)/g,
+      );
+      if (match) {
+        dependencies.push(match[match.length - 1].replace('node_modules/', ''));
+      }
+    });
+    dependencies = dependencies.filter((d) => dependencies.includes(d));
+
+    return dependencies;
+  }
+  return false;
+}
---
+++ test/special/angular.js
@@ -0,0 +1,7 @@
+describe('angular special parser', () => {
+  it('should ignore when filename is not supported"');
+  it('should recognize when path starts with "./node_modules/"');
+  it('should recognize when path starts with "node_modules/"');
+  it('should recognize scoped dependences');
+  it('should recognize unscoped dependences');
+});
