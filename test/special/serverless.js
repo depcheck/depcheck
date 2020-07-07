39c30befe5c9844322a49de9a8536b16c76af26b
Martin Zloch <martin.zloch@md.de>
2020-07-07 10:52:36 +0200

    Feat: Special for serverless config

---
+++ test/special/serverless.js
@@ -0,0 +1,24 @@
+import 'should';
+import parser from '../../src/special/serverless';
+import { getTestParserWithContentPromise } from '../utils';
+
+const testParser = getTestParserWithContentPromise(parser);
+
+describe('serverless special parser', () => {
+  it('should ignore when filename is not supported', async () => {
+    const result = await parser('not-supported.txt', [], '/root/dir');
+    result.should.deepEqual([]);
+  });
+
+  it('should detect serverless when used', async () => {
+    const expected = ['serverless', 'serverless-webpack', 'serverless-offline'];
+    const content = `
+    service:
+      name: serverless-test
+    plugins:
+      - serverless-webpack
+      - serverless-offline`;
+    const actual = await testParser(content, 'serverless.yml');
+    actual.should.deepEqual(expected);
+  });
+});
