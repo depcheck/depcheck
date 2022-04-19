93b61685dc041541964b7b726cd1366cc9d9e290
Miloš Lajtman <mlajtman@slido.com>
2022-04-19 17:50:44 +0200

    feat: use current sass package to parse scss files

--- src/parser/sass.js
+++ src/parser/sass.js
@@ -1,14 +1,8 @@
-import path from 'path';
-import fs from 'fs';
 import lodash from 'lodash';
+import path from 'path';
 import requirePackageName from 'require-package-name';
 
-const { parse } = require('scss-parser');
-const createQueryWrapper = require('query-ast');
 const sass = require('sass');
-
-const IMPORT_RULE_TYPE = 'atrule';
-const IMPORT_KEYWORDS = ['import', 'use', 'forward'];
 
 function unixSlashes(packagePath) {
   return packagePath.replace(/\\/g, '/');
@@ -38,81 +32,32 @@ function removeNodeModulesOrTildaFromPath(packagePath) {
   return suspectedFileName;
 }
 
-function isLocalFile(filePath, folderName) {
-  if (filePath[0] === '_') {
-    return true;
-  }
-
-  if (filePath[0] === '@') {
-    return false;
-  }
-
-  return fs.existsSync(path.join(folderName, `${filePath}.scss`));
-}
-
-function parseSCSS(filename) {
-  const folderName = path.dirname(filename);
-  const fileContents = fs.readFileSync(filename).toString();
-  const ast = parse(fileContents);
-  const queryWrapper = createQueryWrapper(ast);
-  const imports = queryWrapper(IMPORT_RULE_TYPE)
-    .nodes.filter((node) =>
-      IMPORT_KEYWORDS.includes(node.children[0].node.value),
-    )
-    .map((node) => node.children[2].node.value);
-
-  const result = lodash(imports)
-    .filter((packagePath) => packagePath !== filename)
-    .map(unixSlashes)
-    .map(removeNodeModulesOrTildaFromPath)
-    .map(requirePackageName)
-    .uniq()
-    .filter((filePath) => !isLocalFile(filePath, folderName))
-    .filter((x) => x)
-    .value();
-
-  return result;
-}
-
 export default async function parseSASS(filename) {
-  const isScss = path.extname(filename) === '.scss';
-
-  if (isScss) {
-    return parseSCSS(filename);
-  }
-
   const includedFiles = [];
-  let sassDetails = {};
-  try {
-    // sass processor does not respect the custom importer
-    sassDetails = sass.renderSync({
-      file: filename,
-      includePaths: [path.dirname(filename)],
-      importer: [
-        function importer(url) {
-          includedFiles.push(url);
-          return {
-            contents: `
-              h1 {
-                font-size: 40px;
-              }`,
-          };
-        },
-      ],
-    });
-  } catch (e) {
-    sassDetails.stats = {
-      includedFiles,
-    };
-  }
 
-  const result = lodash(sassDetails.stats.includedFiles)
+  sass.compile(filename, {
+    loadPaths: [path.dirname(filename)],
+    importers: [
+      {
+        canonicalize: (importPath) => {
+          includedFiles.push(importPath);
+          return new URL(`file:${importPath}`);
+        },
+        load: () => ({
+          contents: '',
+          syntax: 'css',
+        }),
+      },
+    ],
+  });
+
+  const result = lodash(includedFiles)
     .filter((packagePath) => packagePath !== filename)
     .map(unixSlashes)
     .map(removeNodeModulesOrTildaFromPath)
     .map(requirePackageName)
     .uniq()
-    .filter((x) => x)
+    .filter(Boolean)
     .value();
 
   return result;
