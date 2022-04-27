dec3ee974b7d3b2c4886dddcb3301fee4bb0dcf1
Miloš Lajtman <mlajtman@slido.com>
2022-04-27 18:53:40 +0200

    fix: parse sass and scss modules correctly

--- src/parser/sass.js
+++ src/parser/sass.js
@@ -1,64 +1,70 @@
+import fs from 'fs';
 import lodash from 'lodash';
 import path from 'path';
 import requirePackageName from 'require-package-name';
+import sass from 'sass';
 
-const sass = require('sass');
+const sassModuleRegex = /^\s*@use\s+['"]sass:[a-z]+['"]/gm;
 
-function unixSlashes(packagePath) {
-  return packagePath.replace(/\\/g, '/');
-}
-
-// TODO::remove stuff after ':'
 function removeNodeModulesOrTildaFromPath(packagePath) {
   let suspectedFileName = packagePath;
 
   // remove ':'
-  const colonsIndex = packagePath.indexOf(':');
-  if (colonsIndex > 1) {
-    suspectedFileName = suspectedFileName.substring(0, colonsIndex);
-  }
+  const pathBeforeColon = packagePath.split(':')[0];
+  suspectedFileName = pathBeforeColon ?? suspectedFileName;
+
   // remove 'node_modules/'
-  const nodeModulesIndex = suspectedFileName.indexOf('node_modules/');
-  if (nodeModulesIndex > -1) {
-    return suspectedFileName.substring(
-      nodeModulesIndex + 'node_modules/'.length,
-    );
+  const pathInNodeModules = suspectedFileName.split('node_modules/')[1];
+  if (pathInNodeModules) {
+    return pathInNodeModules;
   }
 
   // remove '~'
-  if (suspectedFileName.indexOf(`~`) === 0) {
-    return suspectedFileName.substring(1);
+  if (suspectedFileName.startsWith(`~`)) {
+    return suspectedFileName.slice(1);
   }
   return suspectedFileName;
 }
 
-export default async function parseSASS(filename) {
-  const includedFiles = [];
+/**
+ * Prevents sass compilation from crashing when importing missing dep
+ * @type {import('sass').Importer}
+ */
+const missingDepImporter = {
+  canonicalize: (importPath) => new URL(`file:${importPath}`),
+  load: () => ({ contents: '', syntax: 'css' }),
+};
 
-  sass.compile(filename, {
-    loadPaths: [path.dirname(filename)],
+export default async function parseSASS(filename, _deps, rootDir) {
+  const modulesDir = path.resolve(rootDir, 'node_modules');
+  const filterLocalFile = (filepath) =>
+    !(filepath.startsWith(rootDir) && !filepath.startsWith(modulesDir));
+
+  const sassString = fs.readFileSync(filename).toString();
+  const usesSass = sassString.match(sassModuleRegex);
+  const { loadedUrls } = sass.compileString(sassString, {
+    url: new URL(`file:${filename}`),
+    syntax: filename.endsWith('scss') ? 'scss' : 'indented',
     importers: [
       {
-        canonicalize: (importPath) => {
-          includedFiles.push(importPath);
-          return new URL(`file:${importPath}`);
+        findFileUrl(url) {
+          const normalizedPath = removeNodeModulesOrTildaFromPath(url);
+          return new URL(normalizedPath, `file:${modulesDir}/`);
         },
-        load: () => ({
-          contents: '',
-          syntax: 'css',
-        }),
       },
+      missingDepImporter,
     ],
   });
 
-  const result = lodash(includedFiles)
-    .filter((packagePath) => packagePath !== filename)
-    .map(unixSlashes)
+  const result = loadedUrls
+    .map((url) => url.pathname)
+    .filter((name) => Boolean(name) && name !== filename)
+    .filter(filterLocalFile)
     .map(removeNodeModulesOrTildaFromPath)
+    // Normalize package name by removing leading slash from URL.pathname
+    .map((name) => (name.startsWith('/') ? name.slice(1) : name))
     .map(requirePackageName)
-    .uniq()
-    .filter(Boolean)
-    .value();
+    .concat(usesSass ? ['sass'] : []);
 
-  return result;
+  return lodash.uniq(result);
 }
