3bc5571f54a8d71b91bb5850751da43ed2ab9a65
Miloš Lajtman <mlajtman@slido.com>
2022-05-03 13:42:06 +0200

    build: remove unused devDependency `node-sass`
    It was stopped being used in this commit, but was never completely removed: https://github.com/depcheck/depcheck/commit/04217610daf58b02509d75e04e589e1666b5fd6c

--- false-alert.js
+++ false-alert.js
@@ -4,19 +4,10 @@
  */
 
 /**
- * These packages are used as optional peer dependencies
- * Because of inconsistenties in npm behavior over time, depcheck
- * cannot model the way these deps are required or used.
- * For more information: https://github.com/depcheck/depcheck/issues/130
- */
-import 'node-sass';
-
-/**
  * Recongnize the required module by nyc. See depcheck/depcheck#183
  */
 import '@babel/polyfill';
 import '@babel/register';
-
 /**
  * TypeScript is loaded using tryRequire and is therefore not detected.
  */
