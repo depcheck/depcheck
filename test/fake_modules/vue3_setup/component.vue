d6f40eac31f0f0beb887bf4032e6be02a730531a
DanEvans485 <danevans485@gmail.com>
2022-08-19 14:52:05 +0100

    Adds vue 3 setup fake module

---
+++ test/fake_modules/vue3_setup/component.vue
@@ -0,0 +1,20 @@
+<script setup>
+import dep1 from 'vue-dep-1';
+import dep2 from 'vue-dep-2';
+
+const message = "Hello World";
+</script>
+
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<style>
+#app {
+  font-size: 18px;
+  font-family: "Roboto", sans-serif;
+  color: blue;
+}
+</style>
