dde9f503333105f011eae27b3ba4d7985d17497c
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2023-04-28 10:13:15 -0500

    Fix depcheck in Svelte files using TypeScript

--- src/parser/svelte.js
+++ src/parser/svelte.js
@@ -2,10 +2,48 @@ import { parse } from '@babel/parser';
 import { getContent } from '../utils/file';
 
 export default async function parseSvelte(filename) {
-  const { compile } = await import('svelte/compiler');
+  const { preprocess } = await import('svelte/compiler');
   const content = await getContent(filename);
-  const { js } = compile(content);
-  return parse(js.code, {
+
+  let script = 'import "svelte";\n';
+  await preprocess(content, [
+    {
+      script: (svelteScript) => {
+        script += svelteScript.content;
+      },
+    },
+  ]);
+
+  return parse(script, {
     sourceType: 'module',
+    // Enable all known compatible @babel/parser plugins at the time of writing.
+    // Because we only parse them, not evaluate any code, it is safe to do so.
+    // note that babel/parser 7+ does not support *, due to plugin incompatibilities
+    plugins: [
+      'typescript',
+      'jsx',
+      'asyncGenerators',
+      'bigInt',
+      'classProperties',
+      'classPrivateProperties',
+      'classPrivateMethods',
+      // ['decorators', { decoratorsBeforeExport: true }],
+      'decorators-legacy',
+      'doExpressions',
+      'dynamicImport',
+      'exportDefaultFrom',
+      'exportNamespaceFrom',
+      'functionBind',
+      'functionSent',
+      'importMeta',
+      'logicalAssignment',
+      'nullishCoalescingOperator',
+      'numericSeparator',
+      'objectRestSpread',
+      'optionalCatchBinding',
+      'optionalChaining',
+      ['pipelineOperator', { proposal: 'minimal' }],
+      'throwExpressions',
+    ],
   });
 }
