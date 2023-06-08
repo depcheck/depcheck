722e615947ece7f57dd0a15c1273a642934bc812
François Hodierne <francois@hodierne.net>
2023-06-08 14:16:34 +0200

    fix: when parsing typescript, only load babel jsx plugin when the file is .tsx

--- src/parser/typescript.js
+++ src/parser/typescript.js
@@ -1,40 +1,44 @@
 import { parse } from '@babel/parser';
 import { getContent } from '../utils/file';
 
+// Enable all known compatible @babel/parser plugins at the time of writing.
+// Because we only parse them, not evaluate any code, it is safe to do so.
+// note that babel/parser 7+ does not support *, due to plugin incompatibilities
+const babelPlugins = [
+  'typescript',
+  'asyncGenerators',
+  'bigInt',
+  'classProperties',
+  'classPrivateProperties',
+  'classPrivateMethods',
+  // ['decorators', { decoratorsBeforeExport: true }],
+  'decorators-legacy',
+  'doExpressions',
+  'dynamicImport',
+  'exportDefaultFrom',
+  'exportNamespaceFrom',
+  'functionBind',
+  'functionSent',
+  'importMeta',
+  'logicalAssignment',
+  'nullishCoalescingOperator',
+  'numericSeparator',
+  'objectRestSpread',
+  'optionalCatchBinding',
+  'optionalChaining',
+  ['pipelineOperator', { proposal: 'minimal' }],
+  'throwExpressions',
+  'importAssertions',
+  'explicitResourceManagement',
+];
+
 export default async function parseTypescript(filename) {
   const content = await getContent(filename);
-  // Enable all known compatible @babel/parser plugins at the time of writing.
-  // Because we only parse them, not evaluate any code, it is safe to do so.
-  // note that babel/parser 7+ does not support *, due to plugin incompatibilities
   return parse(content, {
     sourceType: 'module',
-    plugins: [
-      'typescript',
-      'jsx',
-      'asyncGenerators',
-      'bigInt',
-      'classProperties',
-      'classPrivateProperties',
-      'classPrivateMethods',
-      // ['decorators', { decoratorsBeforeExport: true }],
-      'decorators-legacy',
-      'doExpressions',
-      'dynamicImport',
-      'exportDefaultFrom',
-      'exportNamespaceFrom',
-      'functionBind',
-      'functionSent',
-      'importMeta',
-      'logicalAssignment',
-      'nullishCoalescingOperator',
-      'numericSeparator',
-      'objectRestSpread',
-      'optionalCatchBinding',
-      'optionalChaining',
-      ['pipelineOperator', { proposal: 'minimal' }],
-      'throwExpressions',
-      'importAssertions',
-      'explicitResourceManagement',
-    ],
+    // Only if it's .tsx, we add the jsx plugin as it can cause issues with some regular .ts files
+    plugins: filename.endsWith('.tsx')
+      ? [...babelPlugins, 'jsx']
+      : babelPlugins,
   });
 }
