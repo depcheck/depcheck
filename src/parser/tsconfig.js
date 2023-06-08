0374cac335ae45e83ca6930f125be946a0063805
François Hodierne <francois@hodierne.net>
2023-06-08 13:21:55 +0200

    fix: use JSON5 to parse tsconfig

--- src/parser/tsconfig.js
+++ src/parser/tsconfig.js
@@ -1,10 +1,11 @@
 const requirePackageName = require('require-package-name');
 const { readFileSync } = require('fs');
+const JSON5 = require('json5');
 
 export default function tsconfigParser(filePath, deps) {
   const content = readFileSync(filePath, { encoding: 'utf8' });
   const foundDeps = [];
-  const tsconfigJson = JSON.parse(content);
+  const tsconfigJson = JSON5.parse(content);
   const types = tsconfigJson.compilerOptions?.types;
   if (types) {
     types.forEach((pkg) => {
