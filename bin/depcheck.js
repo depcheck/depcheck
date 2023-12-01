35855e20c021e66635d95783da98e485ebb11b85
François Hodierne <francois@hodierne.net>
2023-12-01 09:23:37 +0100

    ability to use CLI without compiling

--- bin/depcheck.js
+++ bin/depcheck.js
@@ -2,13 +2,16 @@
 
 require('please-upgrade-node')(require('../package.json'));
 
-/* eslint-disable no-console */
+/* eslint-disable no-console, global-require, import/no-extraneous-dependencies */
 
-require('../dist/cli')(
-  process.argv.slice(2),
-  console.log,
-  console.error,
-  function exit(code) {
-    process.exitCode = code;
-  },
-);
+let cli;
+try {
+  cli = require('../dist/cli');
+} catch (e) {
+  require('@babel/register');
+  cli = require('../src/cli');
+}
+
+cli(process.argv.slice(2), console.log, console.error, function exit(code) {
+  process.exitCode = code;
+});
