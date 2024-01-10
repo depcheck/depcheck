1113fed873f95986e5ac71b88df662619fbc9506
Julien Deniau <1398469+jdeniau@users.noreply.github.com>
2024-01-10 12:47:32 +0100

    Typo in comment

--- src/utils/find-line-in-file.js
+++ src/utils/find-line-in-file.js
@@ -8,7 +8,7 @@ export default function findLineInFile(packageJsonContent, jsonKey, needle) {
     return null;
   }
 
-  // lines[0] does contain every line preceding the wanted package. Let's count the "newline" character
+  // lines[1] does contain every line preceding the wanted package. Let's count the "newline" character
   const newlineMatch = lines[1].match(/\n/gm);
 
   // if there is not match, we are probably on a single-line JSON file: everything is on the first line
