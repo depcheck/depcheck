2cf589b1927a3431322acf0c19e4a3e129434abb
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-01-10 09:02:23 +0000

    fix: lint

--- src/utils/module-root.js
+++ src/utils/module-root.js
@@ -10,11 +10,14 @@ export default (...args) => {
   try {
     if (name) {
       const fullpath = resolveFrom(options.cwd, name);
-      const index = fullpath.lastIndexOf(name.replace(/\//g, path.sep))
-      return fullpath.substring(0, index + name.length)
-    } else {
-      return path.dirname(findup('package.json', { cwd: path.dirname(callsites()[1].getFileName()) }))
+      const index = fullpath.lastIndexOf(name.replace(/\//g, path.sep));
+      return fullpath.substring(0, index + name.length);
     }
+    return path.dirname(
+      findup('package.json', {
+        cwd: path.dirname(callsites()[1].getFileName()),
+      }),
+    );
   } catch {
     const pkg = resolveFrom(options.cwd, `${name}/package.json`);
     return path.resolve(path.dirname(pkg));
